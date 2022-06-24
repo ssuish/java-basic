922a7bdc8d6be892b3946d4ccc829e0ea650387b
suisoh <88411618+ssuish@users.noreply.github.com>
2022-06-24 15:44:27 +0800

    oopbasic

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         // Printing Value
-        System.out.println("Hello world!");
-
+        System.out.println("Hello World"); // prints Hello World
     }
 }
---
+++ src/OOP_Basic.java
@@ -0,0 +1,30 @@
+/* Classes and Objects
+ * 
+ * Objects:
+ *      - have states and behaviors. 
+ * Class: 
+ *      - a template/blueprint that describes the 
+ *      behavior/state that the object of its 
+ *      type support.
+ */
+
+ // Variables Types
+public class OOP_Basic {
+    // Class Variables
+    private static int class_var = 0; // static fields.
+    // Instance Variables
+    private int inst_var = 0; // non-static fields.
+
+    // Class Methods
+    public void ToCount() {
+        // Local Variables
+        String msg = "";
+
+        System.out.println(class_var);
+        System.out.println(inst_var);
+    }
+}
+
+class Entity {
+    
+}  
