3a597c0ec3e3f3be567f958ce95067aa71834a45
suisoh <88411618+ssuish@users.noreply.github.com>
2022-06-24 16:13:56 +0800

    oop - added constructor

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         // Printing Value
-        System.out.println("Hello World"); // prints Hello World
+        System.out.println("Hello World\n"); // prints Hello World
+
+        // Calling class and object
+        OOP_Basic obj = new OOP_Basic();
+        // Calling the method inside the class.
+        obj.ToCount();
     }
 }
